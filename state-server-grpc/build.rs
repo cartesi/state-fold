39d34f5ba183534ff471fddf5300a4040a6bc490
Stephen Chen <stephen.chen@cartesi.io>
2021-08-19 23:35:21 +0800

    Move proto file to grpc-interfaces

--- state-server-grpc/build.rs
+++ state-server-grpc/build.rs
@@ -1,5 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("------> Compiling proto file");
-    tonic_build::compile_protos("proto/stateserver.proto")?;
+    tonic_build::compile_protos("../grpc-interfaces/stateserver.proto")?;
     Ok(())
 }
