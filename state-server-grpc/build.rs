dd773367495325c98bbf978219f8369d992e147d
Gabriel Coutinho <gabriel.coutinho@cartesi.io>
2022-05-13 10:38:16 -0300

    Bump version in crates to 0.2.0
    Delete `state-server-grpc` crate.

--- state-server-grpc/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("------> Compiling proto file");
-    tonic_build::compile_protos("../grpc-interfaces/stateserver.proto")?;
-    println!("cargo:rerun-if-changed=.../grpc-interfaces/stateserver.proto");
-    Ok(())
-}
